8afb3ea6d287059daf0490c355c9732c93782280
Ryan567dot <itsbluey1010@gmail.com>
2024-10-13 21:53:41 +0600

    moreob.py

--- More objects/o/moreob.py
+++ More objects/o/moreob.py
@@ -7,3 +7,19 @@ class Human:
 Ryan = Human()
 
 print(Ryan.can_speak)
+
+class Audi:
+    def __init__(self,Name,ID,Salary):
+    self.name = Name
+    self.ID = ID
+    self.Salary = Salary
+    
+    def getdetails(self):
+        print(f"Name :{self.Name}, ID: {self.ID}, Salary: {self.Salary}")
+
+emp1 = Audi("Raj",1234,180000)
+emp2 = Audi("Jevin",4591,150000)
+emp3 = Audi("Arijit",7039,165000)
+
+emp2.getdetails()
+emp3.getdetail()
